0b7e724ca0300e0233642021ce9d0e0b3d43f556
mlebard-galadrim <martinlgaladrim@gmail.com>
2024-04-18 11:57:23 +0200

    rename average function

--- awesome-script.js
+++ awesome-script.js
@@ -15,7 +15,6 @@ function parseNumbers(filename) {
   }
 }
 
-<<<<<<< HEAD
 function computeSum(nums) {
   return nums.reduce((acc, curr) => acc + curr, 0);
 }
@@ -29,12 +28,13 @@ function main() {
   const nums = parseNumbers("numbers.txt");
   const total = computeSum(nums);
   const average = computeAverage(nums);
-=======
+}
+
 function getSum(numbers) {
   return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-function calculateAverage(numbers) {
+function getAverage(numbers) {
   if (numbers.length === 0) return 0;
   return getSum(numbers) / numbers.length;
 }
@@ -42,8 +42,7 @@ function calculateAverage(numbers) {
 function main() {
   const numbers = readNumbers("numbers.txt");
   const total = getSum(numbers);
-  const average = calculateAverage(numbers);
->>>>>>> 104cfbe (rename sum function)
+  const average = getAverage(numbers);
 
   console.log(`Sum: ${total}`);
   console.log(`Average: ${average}`);
